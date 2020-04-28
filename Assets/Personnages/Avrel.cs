3d4a38564f0fb83a66556f367090b00e036e89f5
Bistiboy <nicolas.bisti@ynov.com>
2020-04-28 10:45:12 +0200

    First commit

---
+++ Assets/Personnages/Avrel.cs
@@ -0,0 +1,20 @@
+﻿public class NewBehaviourScript : MonoBehaviour
+{
+
+    [SerializedField] string signe;
+    [SerializedField] string nom;
+    [SerializedField] int positionX;
+    [SerializedField] int positionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Personnages/ChoixRantanplan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labynov
+{
+    public class ChoixRantanplan
+    {
+        public string direction { get; set; }
+
+        public Case @case { get; set; }
+
+        public ChoixRantanplan(string direction , Case @case)
+        {
+            this.direction = direction;
+            this.@case = @case;
+        }
+    }
+}
